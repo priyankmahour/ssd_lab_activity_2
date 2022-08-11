55a458fd7845b67f0d46ff20e9cd902bb92df5f1
PRIYANK MAHOUR <32241835+priyankmahour@users.noreply.github.com>
2022-08-11 16:57:31 +0530

    Update q1.sh

--- q1.sh
+++ q1.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Question 1 Lab 2 SSD
 
-lines=$( echo `awk 'END {print NR}' file1.txt` )
+lines=$( echo `awk 'END {print NR}' $1` )
 #  $lines --> Count the no of lines in file_name == file1.txt 
 
 out_line=`echo "$lines/2" | bc`
@@ -12,4 +12,4 @@ then
     (( out_line++ ))
 fi
 
-awk -v out_line="$out_line" 'NR==out_line{print $0}' file1.txt
+awk -v out_line="$out_line" 'NR==out_line{print $0}' $1
